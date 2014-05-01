ed39f397126fb360484c7b018f52ac69f01ed02e
Dylan Butman <dbutman@nyu.edu>
2014-05-01 10:22:51 -0400

    remove old

--- src/playground/core.clj
+++ src/playground/core.clj
@@ -9,26 +9,4 @@
 (go
  (while true
    (let [devices (<! available-devices)]
-     (pprint :devices (first (vals devices))))))
-
-;; (def mPath "/dev/tty.usbserial-m0000962")
-
-;; ;(def m128 (find-monome mPath))
-
-;; (def monome (monome/connect mPath))
-
-;; (defn on-action
-;;   [action x y state]
-;;   (println action x y state))
-
-;; (handlers/on-action monome on-action :test)
-
-;; ;; (poly/light-led-on-sustain monome)
-
-;; ;; (poly/toggle-all monome)
-
-;; (led/led-on monome 0 0)
-
-;; (monome/connected? monome)
-
-;; (monome/disconnect monome)
+     (pprint (first (vals devices))))))
