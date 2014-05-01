cdddab71575dc469202f92fa71f840a11534f5d8
Dylan Butman <dbutman@nyu.edu>
2014-05-01 10:15:59 -0400

    led actions

--- src/playground/serialosc.clj
+++ src/playground/serialosc.clj
@@ -78,3 +78,32 @@
 ;; (defn toggle-all
 ;;   [state]
 ;;   (osc-send client "/grid/led/all" state))
+;;leds
+
+(defn row->bitmask
+  [row]
+  (Integer/parseInt (apply str row) 2))
+
+(defn set-led
+  [monome x y state]
+  (send-to monome "/grid/led/set" x y state))
+
+(defn set-all
+  [monome state]
+  (send-to monome "/grid/led/all" state))
+
+(defn set-quad
+  "state is a [] size 8 where [[row] ...]"
+  [monome x-off y-off state]
+  (send-to monome "/grid/led/map" x-off y-off (map row->bitmask state)))
+
+(defn set-row
+  "x-off must be a multiple of 8"
+  [monome x-off y state]
+  (send-to monome "/grid/led/row" x-off y (row->bitmask state)))
+
+(defn set-column
+  "x-off must be a multiple of 8"
+  [monome x y-off state]
+  (send-to monome "/grid/led/col" x y-off (row->bitmask state)))
+
